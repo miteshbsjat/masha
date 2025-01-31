788facc1debf3d0a64af643673634acf33214f8e
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2025-01-31 13:31:29 +0530

    Loading configurations from various files complete

---
+++ masha/config_loader.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import yaml
+import json
+import toml
+import configparser
+import argparse
+import sys
+from pathlib import Path
+from typing import Any, Dict
+import logging
+
+from logger_factory import create_logger
+logger = create_logger("masha")
+
+# Function to load configuration files
+def load_config(file_path: Path) -> dict:
+    """
+    Load configuration file into a dictionary.
+
+    Parameters:
+    - file_path (Path): The path to the configuration file.
+
+    Returns:
+    - dict: A dictionary containing the configuration data.
+
+    Raises:
+    - ValueError: If the file type is not supported.
+    """
+    if file_path.suffix == '.yaml' or file_path.suffix == '.yml':
+        with open(file_path, 'r') as f:
+            return yaml.safe_load(f)
+    elif file_path.suffix == '.json':
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    elif file_path.suffix == '.toml':
+        return toml.load(file_path)
+    elif file_path.suffix == '.properties':
+        config = configparser.ConfigParser()
+        config.read(file_path)
+        return {section: dict(config[section]) for section in config.sections()}
+    else:
+        raise ValueError(f"Unsupported file type: {file_path.suffix}")
+
+# Function to merge multiple dictionaries
+def merge_configs(configs: Dict[str, Any]) -> dict:
+    """
+    Merge multiple dictionaries into one.
+
+    Parameters:
+    configs (Dict[str, Any]): A dictionary where each key is a string representing a configuration name,
+                              and the value is another dictionary containing the configuration settings.
+
+    Returns:
+    dict: A single dictionary that contains all the configurations from the input dictionaries.
+          If there are overlapping keys, the values from later dictionaries will overwrite those from earlier ones.
+    """
+    merged_config = {}
+    for config in configs:
+        merged_config.update(config)
+    logger.debug(f"merged_config = {merged_config}")
+    return merged_config
+
+
+def load_and_merge_configs(config_paths: list[Path]):
+    configs = []
+    for config_path in config_paths:
+        try:
+            logger.debug(f"Loading file: {config_path}")
+            config_data = load_config(config_path)
+            configs.append(config_data)
+        except Exception as e:
+            logger.warning(f"Error processing file {config_path}: {e}")
+            return None
+    merged_config = merge_configs(configs)
+    return merged_config
+
+
+# CLI entry point
+def main():
+    parser = argparse.ArgumentParser(description="Validate merged configuration files against a Pydantic model.")
+    parser.add_argument("-v", "--variables", nargs='+', type=Path, required=True, help="Paths to the configuration files.")
+
+    args = parser.parse_args()
+
+    # # Load the model class
+    # model_class = load_model_class(args.model_file, args.class_model)
+    # if not model_class:
+    #     return
+
+    # Load and merge all configuration files
+    merged_config = load_and_merge_configs(args.variables)
+    if not merged_config:
+        return
+
+    print(json.dumps(merged_config))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ masha/logger_factory.py
@@ -0,0 +1,9 @@
+import logging
+import logging.config
+from sys import stdin
+from pathlib import Path
+
+def create_logger(name):
+    log_conf_file = Path(__file__).parent / "logging.conf"
+    logging.config.fileConfig(log_conf_file)
+    return logging.getLogger(name)
