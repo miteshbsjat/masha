fc0ebe97380f959337170a1ac2eb464cecfa9735
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2025-02-04 14:53:57 +0530

    Fixed linting issues in env_loader_py

--- masha/env_loader.py
+++ masha/env_loader.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+Loads the environment variable in configuration value
+"""
+
 import re
 import os
 import json
 from pathlib import Path
+from returns.result import Success, Failure
 import config_loader
 
 from logger_factory import create_logger
@@ -15,13 +20,22 @@ _path_matcher = re.compile(
 )
 
 
-# populate env variable matching ${env_var:default_value} in input dictionary
-def load_env_vars(config: dict) -> dict:
-    # traverse over all keys / subkeys replace env_var recursively
-    pass
+def resolve_env_variables(config) -> dict:
+    """
+    Resolve environment variables in a configuration dictionary.
 
+    This function recursively searches for environment variable placeholders in
+    the given configuration. Placeholders are in the format ${ENV_VAR: default_value},
+    where ENV_VAR is the name of the environment variable and default_value is an
+    optional default value if the environment variable is not set.
 
-def resolve_env_variables(config):
+    Args:
+        config (dict): The configuration dictionary containing potential environment
+                       variable placeholders.
+
+    Returns:
+        dict: A new dictionary with all environment variable placeholders resolved.
+    """
     pattern = re.compile(r"\$\{(\w+):\s*(.*?)\}")  # Match ${ENV_VAR: default_value}
 
     def resolve_value(value):
@@ -42,8 +56,20 @@ def resolve_env_variables(config):
 
 
 def main():
+    """
+    Validates merged configuration files against a Pydantic model.
+
+    This function sets up an argument parser to accept paths to configuration files.
+    It then loads and merges these configuration files, printing the merged result in JSON format.
+    """
     conf_file = Path(__file__).parent.parent / "test" / "env_config.yaml"
-    config = config_loader.load_and_merge_configs([conf_file])
+    config = None
+    match config_loader.load_and_merge_configs([conf_file]):
+        case Success(value):
+            config = value
+        case Failure(value):
+            logger.warning(f"Failed to read configs: {value}")
+            return
     logger.debug(f"config = {config}")
     os.environ["ENV_B"] = "default_not_used_b"
     env_config = resolve_env_variables(config)
