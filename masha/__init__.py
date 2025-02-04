8815f0ad626ee2dcf9295742ed68297556004990
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2025-02-04 18:25:46 +0530

    Added tests for template_renderer

--- masha/__init__.py
+++ masha/__init__.py
@@ -1,4 +1,5 @@
 from .config_loader import load_and_merge_configs, load_config, merge_configs
 from .env_loader import resolve_env_variables
+from .template_renderer import render_templates_with_filters, load_filters_from_directory, load_functions_from_file
 
 __all__ = ('masha')
